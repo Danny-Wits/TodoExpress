3cb72aee83b4f8e36cd7a8bf86fb3507f34771fc
Danny-Wits <dannywits2003@gmail.com>
2026-07-13 15:03:32 +0530

    MVC

--- server.js
+++ server.js
@@ -1,45 +1,21 @@
+const dotenv = require('dotenv')
+dotenv.config();
 const express = require("express");
+const mongoose = require("mongoose");
+const taskRoutes = require('./routes/taskroutes');
+
+mongoose.connect(process.env.DB_STRING,{dbName:'todo'}).then(() => {
+  console.log("Connnect to MongoDB")
+}).catch(err=>console.err(err));
+
 const app = express();
 const PORT = 3000;
-const tasks = [];
-let nextIndex = 1;
-app.use(express.json());
 
-app.get('/frontend', (req, res) => {
+app.use(express.json());
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-app.get("/", (req, res) => {
-  res.json({success: true, data: tasks});
-});
-
-app.post("/", (req, res) => {
-  const title = req.body.title;
-  const task = { id: nextIndex++, title, completed: false };
-  tasks.push(task);
-  console.log(tasks);
-  res.json({ success: true, data: task });
-});
-
-app.patch("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const task = tasks.find(e => e.id === id);
-
-  if(!task) {
-    return res.json({ success: false, message: "Task not found" });
-  }
-  task.completed = !task.completed;
-  res.json({ success: true, data: task });
-})
-
-app.delete('/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const index = tasks.findIndex(e => e.id === id);
-  if(index === -1) {
-    return res.json({ success: false, message: "Task not found" });
-  }
-  const task = tasks.splice(index, 1);
-  res.json({ success: true, data: task });
-} )
+app.use('/api', taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`servers stated on port ${PORT}`);
